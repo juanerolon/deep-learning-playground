b41f5273b8423ee773b5babb4bce1d62f2ae1e99
Juan E Rolon <rolon.math@gmail.com>
2017-11-02 21:59:11 -0500

    fixed face_cascade error

--- pre-trained-model/saved_models/vgg16_predictions.py
+++ pre-trained-model/saved_models/vgg16_predictions.py
@@ -100,6 +100,7 @@ import matplotlib.pyplot as plt
 
 def face_detector(img_path):
     """returns "True" if face is detected in image stored at img_path"""
+    face_cascade = cv2.CascadeClassifier(ds_path + 'haarcascades/haarcascade_frontalface_alt.xml')
     img = cv2.imread(img_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray)
